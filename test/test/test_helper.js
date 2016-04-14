4fc8d0a5948e48f5e96d28649f71f9e085dcdca3
Peng Cheng <chengandpeng@gmail.com>
2016-04-14 10:22:35 +0800

    modify gitignore

--- test/test/test_helper.js
+++ test/test/test_helper.js
@@ -1,35 +1,42 @@
-import _$ from 'jquery';
-import React from 'react';
-import ReactDOM from 'react-dom';
-import TestUtils from 'react-addons-test-utils';
 import jsdom from 'jsdom';
+import jquery from 'jquery';
+import TestUtils from 'react-addons-test-utils';
+import ReactDOM from 'react-dom';
 import chai, { expect } from 'chai';
-import chaiJquery from 'chai-jquery';
+import React from 'react';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
-import reducers from '../src/reducers';
+import reducers from '../src/reducers'; 
+import chaiJquery from 'chai-jquery';
 
+// 设置一个类浏览器的命令行测试环境
 global.document = jsdom.jsdom('<!doctype html><html><body></body></html>');
 global.window = global.document.defaultView;
-const $ = _$(window);
+const $ = jquery(global.window);
 
-chaiJquery(chai, chai.util, $);
+// 创建一个‘renderComponent’函数来render react class
 
-function renderComponent(ComponentClass, props = {}, state = {}) {
-  const componentInstance =  TestUtils.renderIntoDocument(
+function renderComponent(ComponentClass, props, state) {
+// React Add-Ons : renderIntoDocument 
+  const componentInstance = TestUtils.renderIntoDocument(
     <Provider store={createStore(reducers, state)}>
       <ComponentClass {...props} />
     </Provider>
   );
 
-  return $(ReactDOM.findDOMNode(componentInstance));
+  return $(ReactDOM.findDOMNode(componentInstance)); // 生成 HTML => jquery element
 }
 
+
+// 模拟事件simulating events
 $.fn.simulate = function(eventName, value) {
   if (value) {
     this.val(value);
   }
   TestUtils.Simulate[eventName](this[0]);
-};
+}
 
-export {renderComponent, expect};
+// set up chai-jquery
+chaiJquery(chai, chai.util, $);
+
+export { renderComponent, expect };
